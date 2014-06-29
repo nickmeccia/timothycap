1160fba487459a1d4ddddde7fd92f6e262c13c35
nickmeccia <nickmeccia@gmail.com>
2014-06-29 15:22:29 -0500

    Added Roboto Slab as a second webfont.

--- public/javascripts/select.js
+++ public/javascripts/select.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $('li').hide()
+  $('li:first-child').show()
+  $('dd:first-child img').addClass('selected')
 
   $('dd').each(function(idx, elem) {
     $(this).click(function() {
+      $('img').removeClass('selected')
+      $(this).find('img').addClass('selected')
       $('li').hide().eq(idx).show()
     })
   })
